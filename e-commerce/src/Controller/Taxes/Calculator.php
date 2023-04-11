4791602ae974e813976af820959f0888a7446633
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-04-11 20:33:08 +0200

    les limites du contenaire a faire

--- e-commerce/src/Controller/Taxes/Calculator.php
+++ e-commerce/src/Controller/Taxes/Calculator.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller\Taxes;
 
-
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
@@ -11,8 +11,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Calculator extends AbstractController
 {
+    protected $logger;
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     public function calcul(float $prix):float
     {
+        $this->logger->info("un calcul a été fait : $prix");
         return $prix * (20/100);
     }
 }
