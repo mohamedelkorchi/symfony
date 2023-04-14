175a7c8d66c351dc884dc21aa567268521ea4476
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-04-14 17:43:21 +0200

    heritage des templates

--- e-commerce/src/Controller/HelloController.php
+++ e-commerce/src/Controller/HelloController.php
@@ -23,20 +23,15 @@ class  HelloController extends AbstractController
     #[Route("/hello/{prenom?WORLD}", name:"hello")]
 /* SI JE MET LE SERVICE DE CALCUL DANS LA FONCTION, pour laisser le nom world 
 apparaitre je dois le mettre dans le parametre de la route */ 
-    public function hello($prenom="World", Calculator $calculator, Slugify $slugify,
-     Environment $twig, Detector $detector)
+    public function hello($prenom="World", Environment $twig)
     {
         // $this->logger->error("Mon message de log !");
         // $slugify = new Slugify(); 
         /** pour le mettre dans la function il faut le gerer dans
         //  le dossier services.yaml car pas connu du container de sevice, placé dans vendor ! */
 
-        dump($detector->detect(102));
-        dump($detector->detect(20));
-        dump($twig);
-        dump($slugify->slugify("Hello world"));
-        $tva = $calculator->calcul(1400);
-        dump($tva);
-        return new Response("hello $prenom");
+        $html = $twig->render ('hello.html.twig', [
+            'prenom'=> $prenom]);
+        return new Response($html);
     }
 }
