e11c4a0fe0924690dd4a1c9538d79117202fb742
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-04-17 19:40:06 +0200

    premier ex doctrine

--- e-commerce/src/Controller/HomeController.php
+++ e-commerce/src/Controller/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
     public function homepage(): Response
     {
+       
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
