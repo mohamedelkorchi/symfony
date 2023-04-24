3921aaa8762da71a5d0dfa331dddde2e807bd794
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-04-24 16:18:25 +0200

    bug faker

---
+++ e-commerce/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\DBAL\Driver\IBMDB2\Exception\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+
+        for ($p=0; $p <100 ; $p++)
+        { 
+            $product = new Product;
+            $product->setName($faker->__toString())
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug($faker->slug());
+
+            $manager->persist($product);       
+        }
+
+        $manager->flush();
+    }
+}
