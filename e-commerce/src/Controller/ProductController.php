7b910b65be0ca558b8eba3413a9af5df73f16125
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-04-25 16:21:42 +0200

    breadcrumb fait

---
+++ e-commerce/src/Controller/ProductController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/{slug}', name: 'product_category')]
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findOneBy([
+            "slug" => $slug
+        ]);
+        
+        if (!$category) {
+            throw new NotFoundHttpException("La catégorie demandé n'existe pas!");
+        }
+
+        return $this->render('product/category.html.twig', [
+            'controller_name' => 'ProductController',
+            "slug" => $slug,
+            "category" => $category,
+        ]);
+    }
+
+    #[Route('/{category_slug}/{slug}', name: 'product_show')]
+
+    public function show($slug, ProductRepository $productRepository)
+    {
+        $product = $productRepository->findOneBy([
+            "slug" => $slug,
+        ]);
+
+        if(!$product) {
+            throw new NotFoundHttpException("Ce produit n'existe pas!");
+        }
+        return $this->render("product/show.html.twig", [
+            "product" => $product
+        ]);
+    }
+
+}
