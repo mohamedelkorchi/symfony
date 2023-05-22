a42f052e8ccabf3b5bed7ee054fe94de421265ae
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-05-22 16:24:22 +0200

    premier recap

--- e-commerce/src/Controller/CartController.php
+++ e-commerce/src/Controller/CartController.php
@@ -2,15 +2,68 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 
 class CartController extends AbstractController
 {
-    #[Route('/cart/add/{id}', name: 'cart_add')]
-    public function add($id): Response
+    #[Route('/cart/add/{id}', name: 'cart_add', requirements:["id"=>"\d+"])]
+    public function add($id, ProductRepository $productRepository,
+    SessionInterface $session): Response
     {
-      dd($id); 
+        $product = $productRepository->find($id);
+
+        if(!$product)
+        {
+            throw $this->createNotFoundException("Le produit $id n'existe pas !");
+        }
+
+       $cart = $session->get("cart", []);
+
+       if(array_key_exists($id, $cart)) {
+        $cart[$id]++;
+       } else{
+        $cart[$id] = 1;
+       }
+
+       $session->set("cart", $cart);
+       
+       $this->addFlash("success", "Le produit a bien été ajouté au panier :)"); /** raccourcis de la ligne suivante et retiré le flashbaginterface */
+    //    $flashBag->add("success", "Produit bien ajouté");
+       
+
+       return $this->redirectToRoute("product_show", [
+        "category_slug" => $product->getCategory()->getSlug(),
+        "slug" => $product->getSlug()
+       ]);
+    }
+
+    #[Route('/cart', name: 'cart_show')]
+    public function show(SessionInterface $session, ProductRepository $productRepository){
+
+        $detailedCart = [];
+        $total = 0;
+
+        foreach($session->get("cart", []) as $id => $qty){
+            $product = $productRepository->find($id);
+
+            $detailedCart[] = [
+                "product" => $product,
+                "qty" => $qty
+            ];
+
+            $total += ($product->getPrice()* $qty);
+        }
+
+        return $this->render("cart/index.html.twig", [
+            "items" => $detailedCart,
+            "total" => $total
+        ]);
     }
 }
