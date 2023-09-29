947edb21cd19ff7e03224fe1fe8ef2aa2225a72f
mohamedelkorchi <mohamed__elkorchi@hotmail.fr>
2023-09-29 16:01:09 +0200

    modif

--- e-commerce/src/Controller/ProductController.php
+++ e-commerce/src/Controller/ProductController.php
@@ -6,18 +6,11 @@ namespace App\Controller;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
-
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\String\Slugger\SluggerInterface;
-
-
-
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
